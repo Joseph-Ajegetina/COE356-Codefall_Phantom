bdfa41861b0c3a066b2ac29013fa65c95fb4ab70
Steinium7 <51533598+Steinium7@users.noreply.github.com>
2021-07-12 23:45:16 +0000

    Reformating of the route file

--- artisan-bridge/flask/flask_api.py
+++ artisan-bridge/flask/flask_api.py
@@ -1,28 +1,10 @@
 from flask import Flask
 from flask.helpers import flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired
-
- #from forms import LoginForm 
+from forms import LoginForm, signUpForm
 
 app = Flask(__name__)
 # Key to be hashed and hidden in directory
 app.config['SECRET_KEY'] = 'thisisthesecretkeywhichissupposednottobeseen'
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
-    password = PasswordField('poassword', validators=[InputRequired(), Length(min=8, max= 80)])
-    remember = BooleanField('remember me')
-
-class signUpForm(FlaskForm):
-    username = StringField('Username',
-                        validators=[DataRequired(), Length(min=4, max=20)])#minimum username length should be 4
-    email = StringField('Email', validators= [DataRequired(), Email])
-    password = PasswordField('Password', validators=[DataRequired()])# remember to hash password    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    confirm_password = PasswordField('confirm password',
-                            validators=[DataRequired(),EqualTo('password')])
-    submit = SubmitField('Sign Up')
 
 
 @app.route('/')
@@ -31,11 +13,18 @@ class signUpForm(FlaskForm):
 def home_page():
     return "<h1>Home Page</h1>"
 
+
 # Login form, validation and session to be added
 @app.route('/login', methods=['POST','GET'] )
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # delete this line later---------------------------------------------------------------
+        flash(f'Account created for {form.username.data}.')
+        #---------------------------------------------------------------------------------------
+ 
     return "<h1>login page</h1>"
+
 
 @app.route('/register', methods=['GET','POST'] )
 def register():
@@ -45,6 +34,7 @@ def register():
         flash(f'Account created for {form.username.data}.')
         #---------------------------------------------------------------------------------------
     return "<h1>signUp page page</h1>"
+
 
 @app.route('/about')
 def about_page():
