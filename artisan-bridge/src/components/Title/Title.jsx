4932ebc767740133a0b404bd6336b4e35fd11c75
Echis65 <evansatompoya@gmial.com>
2021-07-13 23:10:12 +0000

    Added various components

---
+++ artisan-bridge/src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import "./Title.scss"
+
+export default function Title() {
+    return (
+        <div className="title" id="title">
+            <h1><a href=""> ARTISAN<span className="text-warning">BRIDGE</span></a></h1>
+        </div>
+    )
+}
---
+++ artisan-bridge/src/components/topRatedArtisan/TopRatedArtisan.jsx
@@ -0,0 +1,9 @@
+import "./topRatedArtisan.scss"
+
+export default function topRatedArtisan() {
+    return (
+        <div className="topRatedArtisan">
+            <h4>Top Rated Artisans</h4>
+        </div>
+    )
+}
