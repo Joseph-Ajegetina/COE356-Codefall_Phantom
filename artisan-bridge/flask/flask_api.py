9d6070916138af1caf5809cde9304548300267db
Echis65 <evansatompoya@gmial.com>
2021-07-15 18:21:07 +0000

    Top rated artisan section and footer

--- artisan-bridge/flask/flask_api.py
+++ artisan-bridge/flask/flask_api.py
@@ -1,10 +1,41 @@
 from flask import Flask
+from flask.helpers import flash
+from forms import LoginForm, signUpForm
 
 app = Flask(__name__)
+# Key to be hashed and hidden in directory
+
+app.config['SECRET_KEY'] = 'thisisthesecretkeywhichissupposednottobeseen'
+
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def home_page():
     return "<h1>Home Page</h1>"
+
+
+# Login form, validation and session to be added
+@app.route('/login', methods=['POST','GET'] )
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # delete this line later---------------------------------------------------------------
+        flash(f'Account created for {form.username.data}.')
+        #---------------------------------------------------------------------------------------
+ 
+    return "<h1>login page</h1>"
+
+
+@app.route('/register', methods=['GET','POST'] )
+def register():
+    form = signUpForm()
+    if form.validate_on_submit():
+        # delete this line later---------------------------------------------------------------
+        flash(f'Account created for {form.username.data}.')
+        #---------------------------------------------------------------------------------------
+    return "<h1>signUp page page</h1>"
+
 
 @app.route('/about')
 def about_page():
@@ -17,6 +48,8 @@ def report_page():
 @app.route('/services')
 def Services_page():
     return "<h1>Services Page</h1>"
+
+
 
 
 if __name__ == "__main__":
---
+++ artisan-bridge/flask/forms.py
@@ -0,0 +1,20 @@
+# To contain form classes for data collection and validation
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=4,max=15)])
+    password = PasswordField('poassword', validators=[DataRequired(), Length(min=8, max= 80)])
+    submit = SubmitField('Sign Up')
+    remember = BooleanField('remember me')
+
+class signUpForm(FlaskForm):
+    username = StringField('Username',
+                        validators=[DataRequired(), Length(min=4, max=20)])#minimum username length should be 4
+    email = StringField('Email', validators= [DataRequired(), Email])
+    password = PasswordField('Password', validators=[DataRequired()])# remember to hash password    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    confirm_password = PasswordField('confirm password',
+                            validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
