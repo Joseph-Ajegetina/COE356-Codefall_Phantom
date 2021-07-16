9450d67ae130476a133f19cde7c586e72c1267cf
minnez <patrickackom003@gmail.com>
2021-07-16 19:39:51 +0000

    added

--- artisan-bridge/flask/mysql_connection_test.py
+++ artisan-bridge/flask/mysql_connection_test.py
@@ -3,11 +3,12 @@ from sqlalchemy import create_engine, MetaData
 
 import pymysql
 
-engine = create_engine('mysql+pymysql://root:root@localhost:3306/mysqls')
+engine = create_engine('mysql+pymysql://root:root@localhost:3306/artisanbridge')
 
 connection = engine.connect()
 metadata = db.MetaData()
 
-rounds = db.Table('round', metadata, autoload=True, autoload_with=engine)
+rounds = db.Table('artisans', metadata, autoload=True, autoload_with=engine)
 
-print(connection.execute(db.select([rounds.columns.amount])).fetchall())
+# print(connection.execute(db.select([rounds.columns.amount])).fetchall())
+print(connection.execute("desc services").fetchall())
