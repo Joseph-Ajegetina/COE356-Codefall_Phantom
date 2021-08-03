7b45a6d4a11e910f6d70de96ae722ea5478b19c9
Solomon <snnbotchway@st.knust.edu.gh>
2021-08-03 23:05:40 +0000

    Minor update

--- Database/artisanbridge.sql
+++ Database/artisanbridge.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS services;
 CREATE TABLE services (
   service_id INT NOT NULL AUTO_INCREMENT,
   skill VARCHAR(255) NOT NULL,
-  descriptions VARCHAR(2000) DEFAULT NULL,
+  description VARCHAR(2000) DEFAULT NULL,
   PRIMARY KEY (service_id)
 ); 
 
@@ -16,8 +16,9 @@ CREATE TABLE customers (
   customer_username VARCHAR(255),
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  phone VARCHAR(255) DEFAULT NULL,
-  city VARCHAR(50) NOT NULL,
+  birth_date DATE DEFAULT NULL,
+  contact VARCHAR(255) DEFAULT NULL,
+  address VARCHAR(50) NOT NULL,
   email VARCHAR(255) DEFAULT NULL,
   password VARCHAR(400) NOT NULL,
   profile_image_path VARCHAR(400),
@@ -27,17 +28,19 @@ CREATE TABLE customers (
 DROP TABLE IF EXISTS artisans;
 CREATE TABLE artisans (
   artisan_id INT NOT NULL AUTO_INCREMENT,
+  artisan_username VARCHAR(255),
   service_id INT NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  rating INT ,CONSTRAINT chk_Rating CHECK (rating >= 0 AND rating <=5),
+  rating DECIMAL(2,2) CONSTRAINT chk_Rating CHECK (rating >= 0 AND rating <=5),
   address VARCHAR(255) DEFAULT NULL,
   contact VARCHAR(255) NOT NULL,
+  password VARCHAR(400) NOT NULL,
+  birth_date DATE DEFAULT NULL,
   profile_image_path VARCHAR(400),
   PRIMARY KEY (artisan_id),
   FOREIGN KEY (service_id) REFERENCES services(service_id)
   );
-
 
 DROP TABLE IF EXISTS records;
 CREATE TABLE records (
@@ -68,7 +71,7 @@ ORDER BY artisans.rating DESC
 LIMIT 3;
 
 CREATE VIEW popular_Services AS 
-SELECT count(records.service_id) AS requests, services.skill, services.descriptions 
+SELECT COUNT(records.service_id) AS requests, services.skill, services.description 
 FROM records JOIN services ON records.service_id=services.service_id
 GROUP BY records.service_id
 LIMIT 3;
@@ -79,3 +82,16 @@ ALTER TABLE services AUTO_INCREMENT=1000;
 ALTER TABLE records AUTO_INCREMENT=1000;
 ALTER TABLE admin AUTO_INCREMENT=1000;
 
+INSERT INTO services (skill, description) VALUES ('Electrician', 'A tradesman specializing in electrical wiring of buildings, transmission lines, stationary machines, and related equipment.');
+INSERT INTO services (skill, description) VALUES ('Welder', 'Cuts, shapes and combines materials, mostly metals to make different parts');
+INSERT INTO services (skill, description) VALUES ('Fitter', 'Puts together or installs machinery, engine parts, or other equipment.');
+INSERT INTO services (skill, description) VALUES ('Carpenter', 'Cutting, shaping and installation of building materials during the construction of buildings, ships, timber bridges, concrete formwork, etc.');
+INSERT INTO services (skill, description) VALUES ('Jeweler', 'Makes or repairs jewelery');
+INSERT INTO services (skill, description) VALUES ('Leatherworker', 'Works primarily with leather, crafting leather goods');
+INSERT INTO services (skill, description) VALUES ('Metalworker', 'Monitors, adjusts, and controls various basic or elaborate machines to cut, cast, or mold metals');
+INSERT INTO services (skill, description) VALUES ('Painter', 'Smoothes and prepares surfaces for painting. Paints surfaces, using brushes, spray gun, or paint rollers.');
+INSERT INTO services (skill, description) VALUES ('Potter', 'Creates pots, dishes, mugs, vases, and other types of artwork');
+INSERT INTO services (skill, description) VALUES ('Sculptor', 'Designs and creates three dimensional(3D) forms or models as a way of artistic expression'); 
+INSERT INTO services (skill, description) VALUES ('Weaver', 'Workes in textile production and operates, monitors and maintains highly technical computerised weaving looms to manufacture woven fabrics and materials.');
+INSERT INTO services (skill, description) VALUES ('Plumber', 'Assembles, installs, maintains and pressure tests all pipes, fittings and fixtures of heating, water, drainage, sprinkler, and gas systems according to specifications and plumbing codes.');
+INSERT INTO services (skill, description) VALUES ('Bricklayer/Brickmason', 'Lays bricks, pre-cuts stone and concrete blocks in mortar. Constructs, extends and repairs domestic and commercial buildings and other structures');
