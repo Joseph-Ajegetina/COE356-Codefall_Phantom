3e3caa154c46b8885c42de9c4050ec628de11e4f
Joseph-Ajegetina <josephajongbah@gmail.com>
2021-08-03 11:42:23 +0000

    login and signup authorization and validation

--- artisan-bridge/src/components/navigationBar/Navigation.jsx
+++ artisan-bridge/src/components/navigationBar/Navigation.jsx
@@ -13,7 +13,6 @@ const Navigation = () => {
   useEffect(() =>{
     const storedUserIsLoggedIn = localStorage.getItem("isLoggedIn");
     if (storedUserIsLoggedIn === "1") {
-      console.log("Herre 00")
       setIsLoggedIn(true);
     }
   },[])
@@ -69,55 +68,6 @@ const Navigation = () => {
         </div>
        {isLoggedIn ?<NavLogin/>: <NavPublic/>}
       </div>
-
-      {/*   <div className="left ">
-                <Link to="/home" className="logo">
-                <h1>
-                 ARTISAN<span className="text-warning">BRIDGE</span></h1>
-                </Link>
-            </div>
-            <div className="search">
-          <input
-            type="text"
-            class="searchbar"
-            placeholder="Search for artisans and services"
-          /> <button className="searchicon"> <SearchIcon/> </button>
-          </div>
-            <div className="right">
-            <ul class="nav nav-pills">
-    <li class="nav-item">
-      <Link to="/home" className="nav">
-     <img src="images/home.png" alt="" className="home"/> <span>Home</span>
-      </Link>  
-    </li>
-    <li class="nav-item">
-        <Link to="service" className="nav"> 
-        <img src="images/services.png" alt="" className="services"/> Services
-        </Link>
-    </li>
-    <li class="nav-item">
-        <Link to="/artisan" className="nav">
-        <img src="images/find an artisan.png" alt="" className="services"/> Find an Artisan
-        </Link>
-    </li>
-    <li class="nav-item">
-        <Link to="/records" className="nav">
-        <img src="images/records.png" alt="" className="home"/> Records
-        </Link>
-    </li>
-    <a href="#" className="dropdown" data-toggle="dropdown"><img src="images/profile.png" alt="" className="services"/> Profile</a>
-                <div class="dropdown-menu">
-                    <Link to="/login" className="dropdown-item"> 
-                   Login
-                    </Link>
-                    <Link to="/signup" className="dropdown-item">
-                    Sign up
-                    </Link>
-                    
-                    </div>
-</ul>
-
-            </div> */}
     </div>
   );
 }
