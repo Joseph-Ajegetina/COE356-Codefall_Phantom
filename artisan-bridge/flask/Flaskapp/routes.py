edd4907673ebd041540eeac4ac047003ce58b21b
Joseph-Ajegetina <josephajongbah@gmail.com>
2021-08-16 16:54:18 +0000

    merging to front end

--- artisan-bridge/flask/Flaskapp/routes.py
+++ artisan-bridge/flask/Flaskapp/routes.py
@@ -317,7 +317,7 @@ def report(customer_Id):
     #Establishing connection
     connection = engine.connect()
 
-    query = connection.execute(f"SELECT r1.record_id, artisans.first_name, artisans.last_name, services.skill, r1.date FROM records as r1 INNER JOIN services ON r1.service_id = services.service_id, records as r2 INNER JOIN artisans ON r2.artisan_id = artisans.artisan_id WHERE r1.customer_id = {customer_id} ORDER BY r1.date DESC ").fetchall()
+    query = connection.execute(f"SELECT r1.record_id, artisans.first_name, artisans.last_name, services.skill, r1.date FROM records as r1 INNER JOIN services ON r1.service_id = services.service_id, records as r2 INNER JOIN artisans ON r2.artisan_id = artisans.artisan_id WHERE r1.customer_id = {customer_Id} ORDER BY r1.date DESC ").fetchall()
 
     result = {}
     for i in query:
